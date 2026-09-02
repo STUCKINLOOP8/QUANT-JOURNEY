c44940524a3517e713cf0706c4e4edc8f8970f2b
sambhav <sambhavvvvv10@gmail.com>
2026-09-02 13:59:26 +0530

    Learned Python requests and packages

--- september/packages.py
+++ september/packages.py
@@ -1,9 +1,13 @@
 #packages are the libraries that we will be using in this project. We will be using the following packages:
-#numpy: for numerical computations
-#requests: for making HTTP requests
-#pandas: for data and spreadsheet manipulation
-#openai: for using the OpenAI API
-#beautifulsoup4: to extract data from websites.
+
+"""
+numpy: for numerical computations
+requests: for making HTTP requests
+pandas: for data and spreadsheet manipulation
+openai: for using the OpenAI API
+beautifulsoup4: to extract data from websites.
+
+"""
 
 #--this can be done by using pip install <package_name> in the terminal. For example, to install numpy, you can use the command: pip install numpy.
 
@@ -15,3 +19,6 @@ import requests
 #download a web page
 response = requests.get("https://api.github.com")
 print(response.status_code) #should print 200 if the request was successful
+
+
+
