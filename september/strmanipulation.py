8a6496344fd9dfc163d4c194f8f444a292acd578
sambhav <sambhavvvvv10@gmail.com>
2026-09-02 15:03:58 +0530

    string manipulaton fcntions

---
+++ september/strmanipulation.py
@@ -0,0 +1,17 @@
+name = "Sambhav"
+string = f"hi my name is {name}!"  #f string
+print(string)
+
+
+name = name.lower()
+print(name)
+name = name.upper()
+print (name)
+
+
+sentence = "this is a sentence"
+sentence = sentence.title()  #capitalizes the first letter of each word in the string
+print(sentence)
+
+
+
