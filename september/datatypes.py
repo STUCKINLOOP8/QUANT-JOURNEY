5846f36366ef2c24a2fd90651c594b6c3ac46a59
sambhav <sambhavvvvv10@gmail.com>
2026-09-02 14:18:20 +0530

    learned about data types

---
+++ september/datatypes.py
@@ -0,0 +1,55 @@
+#integer
+a=10
+print(a)
+
+
+
+
+
+#decimal
+b=10.5
+print(b)
+
+
+
+
+
+
+#string
+my_long_string = """hey 
+this is a long string
+that spans multiple lines"""
+
+str="hello"
+
+print(my_long_string)
+print(str)
+
+#concatination of strings
+first_name = "sambhav"
+last_name = "kumar"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+long_dash = "-" * 30
+print(long_dash)
+
+#to find length of the string
+print(len(full_name))
+
+
+
+
+
+#booleans:
+#direct assignment
+is_logged_in = True
+print(is_logged_in)
+
+#from comparison
+age = 18
+can_vote = age >= 18
+print(can_vote)
+
+
