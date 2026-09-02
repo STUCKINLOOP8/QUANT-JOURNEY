b10ee56b821e4d5ed37bdb0a155b7cb0ba44362d
sambhav <sambhavvvvv10@gmail.com>
2026-09-02 15:44:03 +0530

    basics

--- september/basics.py
+++ september/basics.py
@@ -23,5 +23,13 @@ print("Lets do this!!")
 print('*' * 10)
 
 
+#to take an input from user:
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+c=a+b
+print("The sum of the two numbers is: ", c)
+
+
+
 
 
