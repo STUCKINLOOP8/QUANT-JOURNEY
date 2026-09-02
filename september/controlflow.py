7c1e26c9fc2c3d18a9fa58ee645427f7c458f68b
sambhav <sambhavvvvv10@gmail.com>
2026-09-02 15:26:14 +0530

    learned about the control statements

---
+++ september/controlflow.py
@@ -0,0 +1,32 @@
+#1
+temperature = 25
+
+if temperature > 30:
+    print("its very hot")
+        
+elif temperature > 25:
+    print("its hot")
+    
+else:
+    print("its not hot")
+    
+    
+#2
+has_ticket = True
+age = 18
+
+if has_ticket:
+    if age>=18:
+        print("enjoy the movie")
+    else:
+        print("needs supervision")
+        
+else:
+    print("please buy a ticket")
+    
+    
+    
+    
+
+    
+  
